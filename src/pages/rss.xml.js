a5bbb07f2043f875fbd71acacb282ce21a14d821
moshyfawn <oleg.butorov.98@gmail.com>
2022-12-22 20:11:06 -0500

    ✨ feat: add rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '../config'
+
+export const get = async () => {
+	const blogPosts = await getCollection('blog')
+
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: SITE_URL,
+		items: blogPosts.map(post => ({
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.date,
+			link: post.slug
+		}))
+	})
+}
