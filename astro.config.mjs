0c782a0e71f9f18a70941a454a068f873eca61bb
moshyfawn <oleg.butorov.98@gmail.com>
2022-12-22 20:06:03 -0500

    ✨ feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://moshyfawn.dev',
 	integrations: [
+		sitemap(),
 		tailwind(),
 	]
 })
