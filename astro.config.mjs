cb4e2d41a1a4c0c046ed804ab4e22b37f2a25232
moshyfawn <oleg.butorov.98@gmail.com>
2022-12-22 20:07:14 -0500

    ✨ feat: add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
+	experimental: {
+		contentCollections: true
+	},
 	site: 'https://moshyfawn.dev',
 	integrations: [
 		sitemap(),
 		tailwind(),
+		mdx({
+			extendPlugins: 'astroDefaults'
+		})
 	]
 })
