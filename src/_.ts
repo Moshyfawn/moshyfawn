1d383a7770a5937bf7c07c22e37861c0a6fd67a7
moshyfawn <oleg.butorov.98@gmail.com>
2022-12-22 20:09:26 -0500

    ✨ feat: add seo

--- src/_.ts
+++
@@ -1,2 +0,0 @@
-// INFO: temp workaround to fix "No inputs were found in config file"
-export const _ = void 0;
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const MY_NAME = 'moshyfawn'
+export const TWITTER_HANDLE = '@moshyfawn'
+
+export const SITE_TITLE = "Moshyfawn's Corner"
+export const SITE_DESCRIPTION =
+	'An all things moshyfawn place on the Internet; primarily related but not limited to programming'
+export const SITE_URL = import.meta.env.SITE
