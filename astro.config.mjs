e0cce37e4c07e68a4af5e4153850472c18ad99ad
moshyfawn <oleg.butorov.98@gmail.com>
2023-01-02 13:20:54 -0500

    ✨ feat(Blog): add links enhancements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
+
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,11 +17,30 @@ export default defineConfig({
 			__APP_VERSION__: JSON.stringify(process.env.npm_package_version)
 		}
 	},
+	markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: 'wrap',
+					properties: { class: 'no-underline' },
+					test: ['h2', 'h3', 'h4', 'h5', 'h6']
+				}
+			],
+			[
+				rehypeExternalLinks,
+				{
+					rel: [],
+					content: {
+						type: 'text', value: ' ↗'
+					}
+				}
+			]
+		]
+	},
 	integrations: [
 		sitemap(),
-		tailwind(),
-		mdx({
-			extendPlugins: 'astroDefaults'
-		})
+		tailwind()
 	]
 })
