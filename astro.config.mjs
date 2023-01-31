b8e77e822f3fdcb96c359f06f142306590f2d26e
Justin Sun <i@jsun969.cn>
2023-02-01 01:13:40 +0800

    :bug: fix(Blog): enable `no-underline` for heading links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeSlug from 'rehype-slug'
 import rehypeExternalLinks from 'rehype-external-links'
+import rehypeSlug from 'rehype-slug'
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,7 +24,6 @@ export default defineConfig({
 				rehypeAutolinkHeadings,
 				{
 					behavior: 'wrap',
-					properties: { class: 'no-underline' },
 					test: ['h2', 'h3', 'h4', 'h5', 'h6']
 				}
 			],
